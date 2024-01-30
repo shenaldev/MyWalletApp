34ebdb708e1a6bba65ba94307debfea53f2618af
Shenal Fernando <developershenal@gmail.com>
2024-01-30 21:56:26 +0530

    Refactor App.css and add AuthLayout to AuthRoot

---
+++ src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type PropTypes = {
+  children?: React.ReactNode;
+  wrapperClass?: string;
+  bodyClass?: string;
+};
+
+function AuthLayout({
+  children,
+  wrapperClass = "",
+  bodyClass = "",
+}: PropTypes) {
+  return (
+    <div className={`h-[100dvh] w-full bg-slate-100 ${wrapperClass}`}>
+      <div className="flex justify-center pt-8">
+        <h1>Logo Here</h1>
+      </div>
+      <div className={` ${bodyClass}`}>{children}</div>
+    </div>
+  );
+}
+
+export default AuthLayout;
--- src/components/roots/AuthRoot.tsx
+++ src/components/roots/AuthRoot.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
+import AuthLayout from "../layouts/AuthLayout";
 
 function AuthRoot() {
   const auth = useAuth();
   console.log(auth);
-  return <Outlet />;
+  return (
+    <>
+      <AuthLayout>
+        <Outlet />
+      </AuthLayout>
+    </>
+  );
 }
 
 export default AuthRoot;
