529662018690b37ab3574e0ea9ad35f61ff22142
Shenal Fernando <developershenal@gmail.com>
2024-02-27 02:29:09 +0530

    move monthyear provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ import {
 } from "./pages/Pages";
 import { ThemeProvider } from "./components/providers/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MonthYearProvider from "./components/providers/MonthYearProvider";
 
 //ROUTER
 const router = createBrowserRouter([
@@ -47,7 +48,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <ThemeProvider defaultTheme="light" storageKey="theme">
-          <RouterProvider router={router} />
+          <MonthYearProvider>
+            <RouterProvider router={router} />
+          </MonthYearProvider>
         </ThemeProvider>
       </AuthProvider>
     </QueryClientProvider>
