03fff0176e1297c139991ef134e9003b8fa30f66
Shenal Fernando <developershenal@gmail.com>
2024-03-03 02:19:50 +0530

    add classname prop

--- src/components/elements/ServerErrorAlert.tsx
+++ src/components/elements/ServerErrorAlert.tsx
@@ -1,7 +1,15 @@
 import { AlertCircle } from "lucide-react";
 import { Alert, AlertDescription } from "../ui/alert";
+import { cn } from "@/lib/utils";
 
-function ServerErrorAlert({ errors }: { errors: Error | any[] | null }) {
+type ServerErrorAlertProps = {
+  errors: Error | any[] | null;
+  className?: string;
+};
+
+function ServerErrorAlert({ errors, className }: ServerErrorAlertProps) {
+  const alertClasses = cn("", className);
+
   const errorList = Array.isArray(errors)
     ? errors.map((error, index) => (
         <li key={index} className="text-sm">
@@ -13,7 +21,7 @@ function ServerErrorAlert({ errors }: { errors: Error | any[] | null }) {
   errors == null || (typeof Error && <li>Unexpected error occurred!</li>);
 
   return (
-    <Alert variant="destructive" className="mb-4">
+    <Alert variant="destructive" className={alertClasses}>
       <AlertCircle className="h-4 w-4" />
       <AlertDescription>
         <ul>{errorList}</ul>
